c52ec78c1c78836b4158562a8552ff9bc22974be
xu <han.xu5@husky.neu.edu>
2018-07-27 12:35:48 -0400

    Get steps done

--- pages/game/game.js
+++ pages/game/game.js
@@ -11,25 +11,7 @@ Page({
     },
     getSteps: function () {
         var that = this;
-        // wx.getWeRunData({
-        //     success(resRun) {
-        //         const encryptedData = resRun;
-        //         console.log(encryptedData);
-        //         wx.request({
-        //             url: 'http://localhost:51000/api/values/'+'123',
-        //             data: {
-        //                 encryptedData: resRun.encryptedData,
-        //                 iv: resRun.iv
-        //             },
-        //             method: 'POST',
-        //             success: function (resDecrypt) {
-        //                 console.log(resDecrypt);
-        //                 // var runData = JSON.parse(resDecrypt.data.data);
-        //                 // console.log(runData);
-        //             }
-        //         });
-        //     }
-        // });
+        
         var secret = "Secret tst";
         wx.login({
             success: function(resLogin){
@@ -38,12 +20,32 @@ Page({
                         url: "https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code",
                         data:{
                             js_code : resLogin.code,
-                            appid: "wx26f098a4803f8d2e",
-                            secret: this.secret,
+                            appid: "wx1730947c98cfd8ad",
+                            secret: "e91a300b390e45e4133cea893b7fca0c",
                             grant_type: "authorization_code"
                         },
                         success: function(resSession){
-                            console.log(resSession);
+                            console.log(resSession);   //TODO DELETE
+                            wx.getWeRunData({
+                                success(resRun) {
+                                    const encryptedData = resRun;
+                                    console.log(encryptedData);
+                                    wx.request({
+                                        url: 'http://localhost:51000/api/values/'+'123',
+                                        data: {
+                                            encryptedData: resRun.encryptedData,
+                                            iv: resRun.iv,
+                                            sessionKey: resSession.data.session_key
+                                        },
+                                        method: 'POST',
+                                        success: function (resDecrypt) {
+                                            console.log(resDecrypt.data.stepInfoList[0].step);
+                                            // var runData = JSON.parse(resDecrypt.data.data);
+                                            // console.log(runData);
+                                        }
+                                    });
+                                }
+                            });
 
                         }
                     });
